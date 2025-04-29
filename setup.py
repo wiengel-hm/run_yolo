96ba912263b18d8028038725f489f5db2d6c75e9
william-mx <william.engel@mdynamix.de>
2025-04-29 05:39:18 +0200

    init node

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
@@ -20,6 +19,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'run_yolo = run_yolo.run_yolo:main'
         ],
     },
 )
